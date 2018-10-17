12f18d36ed51db0901fc95a4f0c619e4abb5cae6
Brian Campbell <v-bricam@microsoft.com>
2018-10-17 13:34:17 -0700

    testing structs

--- logger.py
+++ logger.py
@@ -1,18 +1,19 @@
 # Imports the Google Cloud client library
 from google.cloud import logging
 
+
 # Instantiates a client
 logging_client = logging.Client()
 
 # The name of the log to write to
-log_name = 'my-log'
+log_name = 'chamber'
 # Selects the log to write to
 logger = logging_client.logger(log_name)
 
 # The data to log
-text = 'Hello, world!'
+struct = {'Humidity': 0, 'Temp', 0}
 
 # Writes the log entry
-logger.log_text(text)
+logger.log_struct(struct)
 
 print('Logged: {}'.format(text))
