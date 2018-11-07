5d41c97672c3f2e4b9d5b9e2d9fe065770b71887
chicken <delicious.chicken@gmail.com>
2018-11-06 21:02:26 -0800

    logging update

--- main.py
+++ main.py
@@ -20,109 +20,109 @@ min_hum = 85
 max_hum = 92
 
 class RepeatedTimer(object):
-    def __init__(self, interval, function, *args, **kwargs):
-        self._timer     = None
-        self.interval   = interval
-        self.function   = function
-        self.args       = args
-        self.kwargs     = kwargs
-        self.is_running = False
-        self.start()
+	def __init__(self, interval, function, *args, **kwargs):
+		self._timer	 = None
+		self.interval   = interval
+		self.function   = function
+		self.args	   = args
+		self.kwargs	 = kwargs
+		self.is_running = False
+		self.start()
 
-    def _run(self):
-        self.is_running = False
-        self.start()
-        self.function(*self.args, **self.kwargs)
+	def _run(self):
+		self.is_running = False
+		self.start()
+		self.function(*self.args, **self.kwargs)
 
-    def start(self):
-        if not self.is_running:
-            self._timer = Timer(self.interval, self._run)
-            self._timer.start()
-            self.is_running = True
+	def start(self):
+		if not self.is_running:
+			self._timer = Timer(self.interval, self._run)
+			self._timer.start()
+			self.is_running = True
 
-    def stop(self):
-        self._timer.cancel()
-        self.is_running = False
+	def stop(self):
+		self._timer.cancel()
+		self.is_running = False
 
 def status(id):
-    return vsapi.get_detail(id)['deviceStatus']
+	return vsapi.get_detail(id)['deviceStatus']
 
 def heat_off():
-    if status(heat_id) == 'on':
-        vsapi.turn_off(heat_id)
-    return
+	if status(heat_id) == 'on':
+		vsapi.turn_off(heat_id)
+	return
 
 def heat_on():
-    if status(heat_id) == 'off':
-        print('HEATING')
-        vsapi.turn_on(heat_id)
-    return
+	if status(heat_id) == 'off':
+		print('HEATING')
+		vsapi.turn_on(heat_id)
+	return
 
 def fan_off():
-    if status(fan_id) == 'on':
-        vsapi.turn_off(fan_id)
-    return
+	if status(fan_id) == 'on':
+		vsapi.turn_off(fan_id)
+	return
 
 def fan_on():
-    if status(fan_id) == 'off':
-        print('FANNING')
-        vsapi.turn_on(fan_id)
-    return
+	if status(fan_id) == 'off':
+		print('FANNING')
+		vsapi.turn_on(fan_id)
+	return
 
 def humidifier_off():
-    if status(humid_id) == 'on':
-        vsapi.turn_off(humid_id)
-    return
+	if status(humid_id) == 'on':
+		vsapi.turn_off(humid_id)
+	return
 
 def humidifier_on():
-    if status(humid_id) == 'off':
-        print('FOGGING')
-        vsapi.turn_on(humid_id)
-    return
+	if status(humid_id) == 'off':
+		print('FOGGING')
+		vsapi.turn_on(humid_id)
+	return
 
 def measure():
 	state = GPIO.input(trans)
 	if state == False:
 		GPIO.output(trans, 1)
 		time.sleep(1)
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-    if humidity is None or temperature is None:
-    	print('bad sensor read')
-    	GPIO.output(trans, 0)
-    	fan_off()
-    	return
-    ftemp = (temperature * 1.8) + 32
-    my_logger.log(humidity, ftemp)
-    
-    if ftemp > max_temp:
-        print('too hot:{}'.format(ftemp))
-        heat_off()
-        fan_on()
-    elif ftemp < min_temp:
-        print('too cold:{}'.format(ftemp))
-        heat_on()
-        fan_off()
-    else:
-    	print('perfect temp:{}'.format(ftemp))
-    	heat_off()
-    	fan_off()
+	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+	if humidity is None or temperature is None:
+		print('bad sensor read')
+		GPIO.output(trans, 0)
+		fan_off()
+		return
+	ftemp = (temperature * 1.8) + 32
+	my_logger.log(humidity, ftemp)
+	
+	if ftemp > max_temp:
+		print('too hot:{}'.format(ftemp))
+		heat_off()
+		fan_on()
+	elif ftemp < min_temp:
+		print('too cold:{}'.format(ftemp))
+		heat_on()
+		fan_off()
+	else:
+		print('perfect temp:{}'.format(ftemp))
+		heat_off()
+		fan_off()
 
-    if humidity > max_hum:
-        print('too humid:{}'.format(humidity))
-        humidifier_off()
-        fan_on()
-    elif humidity < min_hum:
-        print('too dry:{}'.format(humidity))
-        fan_off()
-        humidifier_on()
-    else:
-    	print('perfect moint:'.format(humidity))
-    	humidifier_off()
-    	fan_off()
+	if humidity > max_hum:
+		print('too humid:{}'.format(humidity))
+		humidifier_off()
+		fan_on()
+	elif humidity < min_hum:
+		print('too dry:{}'.format(humidity))
+		fan_off()
+		humidifier_on()
+	else:
+		print('perfect moint:'.format(humidity))
+		humidifier_off()
+		fan_off()
 
 print ("starting...")
 print ("communicating with vsapi:")
 for dev in vsapi.get_devices():
-    print ('{}:{}:{}'.format(dev['deviceName'],dev['cid'],dev['connectionStatus']))
+	print ('{}:{}:{}'.format(dev['deviceName'],dev['cid'],dev['connectionStatus']))
 my_logger = Logger('chamber')
 rt = RepeatedTimer(30, measure) 
